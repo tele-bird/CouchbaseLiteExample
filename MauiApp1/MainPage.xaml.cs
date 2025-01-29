d1075d2704651d46998d3b9d0f9da8be4be4998f
Phil Hadley <phadley@junipercommerce.com>
2025-01-29 06:00:56 -0700

    added project

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using Couchbase.Lite;
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		try
+		{
+			var db = new Database("TestDatabase");
+			ErrorLabel.Text = $"created {db.Name} at {db.Path}";
+		}
+		catch(Exception exception)
+		{
+			StringBuilder sbResult = new StringBuilder();
+			Exception? exc = exception;
+			int level = 0;
+			while(exc != null)
+			{
+				sbResult.AppendLine($"LEVEL: {level} {exc.GetType().FullName}: {exc.Message} {exc.StackTrace}");
+				sbResult.AppendLine();
+				exc = exc.InnerException;
+				++level;
+			}
+			ErrorLabel.Text = $"{sbResult}";
+		}
+	}
+}

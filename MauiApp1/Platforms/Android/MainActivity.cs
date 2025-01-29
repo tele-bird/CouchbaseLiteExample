c54380f20dc70b218750e092e1945b34b06f5d21
Phil Hadley <phadley@junipercommerce.com>
2025-01-29 07:46:26 -0700

    activate() for Android

--- MauiApp1/Platforms/Android/MainActivity.cs
+++ MauiApp1/Platforms/Android/MainActivity.cs
@@ -7,4 +7,9 @@ namespace MauiApp1;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        Couchbase.Lite.Support.Droid.Activate(this);
+    }
 }
